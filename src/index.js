e3a453ae2abbba15f3a8846d4eb59a17abb6cbc9
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-04-01 17:57:21 +0100

    pass the onclick function via props from board to square, remove state from square component as it will be kept in the parent board component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {value: null};
-  }
 
   render() {
     return (
-      <button className="square" onClick= {() => this.setState({value : "X"})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -26,9 +22,11 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square value={this.state.squares[i]} 
+                   onClick={ () => this.handleClick(i)} />
+    )
   }
-
   render() {
     const status = 'Next player: X';
 
